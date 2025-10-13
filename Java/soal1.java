175ffe8fdc0a8f9ee84543dc326a046b7d25095f
TubagusMuhamadIhsanFebrian <tubagusihsan28@gmail.com>
2025-10-13 20:42:44 +0700

    C++
    tugas C++

---
+++ Java/soal1.java
@@ -0,0 +1,13 @@
+public class soal1 {
+    public static void main(String[] args) {
+        String nama = "Tubagus";
+        int umur = 18;
+        double tinggi = 162;
+        boolean aktif = true;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Aktif: " + aktif);
+    }
+}   
---
+++ PHP/soal1.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "Tubagus";
+$umur = 18; 
+$tinggi = 162;
+$aktif = true;
+
+echo "Nama: $nama\n";
+echo "Umur: $umur\n";
+echo "Tinggi: $tinggi\n";
+echo "Status aktif: $aktif\n";
+?>
